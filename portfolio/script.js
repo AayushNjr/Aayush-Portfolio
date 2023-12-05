fec6f79d6ce82d5361f314b0568adaa77f07b43c
Aayush_Njr <>
2023-12-05 18:15:02 +0530

    Aayush_njr

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hampizza-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+function redirectTo() {
+    window.location.href="contact.html","_blank";
+}
+    
+    
